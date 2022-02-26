25f18865943a26b275d61f190656fc359f55f784
Johan Tovar <jtovarto@gmail.com>
2022-02-26 19:52:05 -0400

    add redux provider in main component

--- todo-drag/src/main.jsx
+++ todo-drag/src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import { Provider } from "react-redux";
+import store from "./redux/store/index.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
