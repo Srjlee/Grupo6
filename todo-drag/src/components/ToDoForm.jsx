a343a652ea930f67c98c3378089be20f61f02a0a
Johan Tovar <jtovarto@gmail.com>
2022-02-27 13:37:34 -0400

    add csss transition to ToDoComponent

--- todo-drag/src/components/ToDoForm.jsx
+++ todo-drag/src/components/ToDoForm.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { addTodo } from "../redux/actions";
+import { CSSTransition } from "react-transition-group";
 
-const ToDoForm = () => {
+const ToDoForm = ({ toggleFormShow, showForm }) => {
   const toDoDefault = { task: "", description: "" };
   const dispatch = useDispatch();
   const [toDo, setToDo] = useState(toDoDefault);
-
+  const btnRef = useRef(null);
   const onChangeHandler = (e) => {
     setToDo({
       ...toDo,
@@ -15,55 +16,91 @@ const ToDoForm = () => {
   };
 
   const onSubmitHandler = (e) => {
-    e.preventDefault();
+    e.preventDefault();    
     dispatch(addTodo(toDo));
     setToDo(toDoDefault);
   };
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "k" && e.ctrlKey && e.altKey) {
+        toggleFormShow(true);
+      }
+      if (e.key === "Escape") {
+        toggleFormShow(false);
+      }
+      if (e.keyCode === 13) {
+        e.preventDefault();        
+        btnRef.current.click();
+      }
+    });
+  }, []);
 
   return (
     <>
-      <div className="card w-50 mx-auto shadow-lg">
-        <h5 className="card-header bg-primary text-light">Featured</h5>
-        <div className="card-body">
-          <form onSubmit={onSubmitHandler}>
-            <div className="mb-3">
-              <label htmlFor="task" className="form-label">
-                Tarea
-              </label>
-              <input
-                id="task"
-                name="task"
-                value={toDo.task}
-                onChange={onChangeHandler}
-                type="text"
-                required
-                className="form-control"
-                placeholder="ej. Hacer la homework"
-              />
+      <CSSTransition
+        in={showForm}
+        timeout={500}
+        classNames="fade"
+        unmountOnExit
+      >
+        <div className="wrapper-modal">
+          <div className="overlay"></div>
+          <div className="card col-10 col-md-6 col-lg-4 mx-auto shadow-lg">
+            <h5 className="card-header bg-primary text-light">Featured</h5>
+            <div className="card-body">
+              <form onSubmit={onSubmitHandler}>
+                <div className="mb-3">
+                  <label htmlFor="task" className="form-label">
+                    Tarea
+                  </label>
+                  <input
+                    id="task"
+                    name="task"
+                    value={toDo.task}
+                    onChange={onChangeHandler}
+                    type="text"
+                    required
+                    className="form-control"
+                    placeholder="ej. Hacer la homework"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="description" className="form-label">
+                    Descripción
+                  </label>
+                  <textarea
+                    id="description"
+                    name="description"
+                    value={toDo.description}
+                    onChange={onChangeHandler}
+                    required
+                    className="form-control"
+                    placeholder="Agregue un breve descripción para su tarea"
+                    rows="3"
+                  ></textarea>
+                </div>
+                <div className="d-flex justify-content-around  gap-2">
+                  <button
+                    type="button"
+                    onClick={() => toggleFormShow(false)}
+                    className="btn btn-outline-primary btn-block"
+                  >
+                    Cancelar
+                  </button>
+                  <button
+                    ref={btnRef}
+                    type="submit"
+                    className="btn btn-primary btn-block"
+                    de
+                  >
+                    Agregar
+                  </button>
+                </div>
+              </form>
             </div>
-            <div className="mb-3">
-              <label htmlFor="description" className="form-label">
-                Descripción
-              </label>
-              <textarea
-                id="description"
-                name="description"
-                value={toDo.description}
-                onChange={onChangeHandler}
-                required
-                className="form-control"
-                placeholder="Agregue un breve descripción para su tarea"
-                rows="3"
-              ></textarea>
-            </div>
-            <div className="d-grid gap-2">
-              <button type="submit" className="btn btn-primary btn-block">
-                Agregar Tarea
-              </button>
-            </div>
-          </form>
+          </div>
         </div>
-      </div>
+      </CSSTransition>
     </>
   );
 };
