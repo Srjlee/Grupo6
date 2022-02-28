79528a92e63c06fc4c257e956f7957113ebecc5d
Johan Tovar <jtovarto@gmail.com>
2022-02-27 23:04:39 -0400

    add reducer file

--- todo-drag/src/redux/reducer/index.js
+++ todo-drag/src/redux/reducer/index.js
@@ -1,14 +1,44 @@
-import { ADD_TODO, REMOVE_TODO } from "../actions";
-const initialState = [];
+import { nanoid } from "nanoid";
+import {
+  PUSH_NOTIFICATION,
+  ADD_TODO,
+  REMOVE_TODO,
+  REMOVE_NOTIFICATION,
+} from "../actions";
+
+const initialState = {
+  todos: [],
+  notifications: [],
+};
 
 const reducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    case ADD_TODO: {      
-      return [...state, payload];
+    case ADD_TODO: {
+      const todo = { id: nanoid(), ...payload, status: "todo" };
+      return {
+        ...state,
+        todos: [...state.todos, todo],
+      };
     }
-    case REMOVE_TODO: {
+
+    case PUSH_NOTIFICATION: {
+      const notification = { id: nanoid(), ...payload };
+      return {
+        ...state,
+        notifications: [...state.notifications, notification],
+      };
+    }
+
+    case REMOVE_NOTIFICATION: {
+      return {
+        ...state,
+        notifications: state.notifications.slice(1),
+      };
+    }
+
+    /*  case REMOVE_TODO: {
       return [...state, state.filter((todo) => todo !== payload)];
-    }
+    } */
     default:
       return state;
   }
