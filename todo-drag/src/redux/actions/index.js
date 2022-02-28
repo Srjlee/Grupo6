43649e2cf44622a07f0fe538dde2d35489fd8767
Johan Tovar <jtovarto@gmail.com>
2022-02-27 23:04:25 -0400

    add actions file

--- todo-drag/src/redux/actions/index.js
+++ todo-drag/src/redux/actions/index.js
@@ -1,6 +1,8 @@
 export const ADD_TODO = "add_todo";
 export const REMOVE_TODO = "remove_todo";
 export const UPDATE_TODO = "update_todo";
+export const PUSH_NOTIFICATION = "push_notification";
+export const REMOVE_NOTIFICATION = "remove_notification";
 
 export const addTodo = (toDo) => ({
   type: ADD_TODO,
@@ -18,4 +20,13 @@ export const updateTodo = (id, todo) => ({
     id: id,
     data: todo,
   },
+});
+
+export const pushNotification = (notification) => ({
+  type: PUSH_NOTIFICATION,
+  payload: notification,
+});
+
+export const removeNotification = () => ({
+  type: REMOVE_NOTIFICATION,
 });
